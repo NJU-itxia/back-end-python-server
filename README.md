f2e86f838304d8d71cd340566f98e1b3730b7d74
FengChendian <njuzhaoyichen@gmail.com>
2018-10-24 15:54:17 +0800

    commit message

---
+++ README.md
@@ -0,0 +1,8 @@
+# back-end-python-server
+## 文件介绍
+- venv是一个python的虚拟环境
+- dysms_python 是aliyun的接口文件
+
+## 使用方法
+1. 修改dysms——python文件夹中的api文件参数，详见阿里云官方文档
+2. 执行python_backend_api.py
---
+++ python_backend_api.py
@@ -0,0 +1,36 @@
+# Flask 1.0.2
+# Flask_restful 0.3.6-2
+# Python 3.7.0
+
+from flask import Flask, request
+from flask_restful import Resource,Api
+import re
+from dysms_python import demo_sms_send
+
+
+app = Flask(__name__)
+api = Api(app)
+
+#使JSON中文不乱码
+app.config.update(RESTFUL_JSON=dict(ensure_ascii=False))
+
+SMS_ID = 0
+SMS_KEY = "none"
+SMS_SIGN = 'none'
+
+class SMS_API(Resource):
+    # def get(self, num):
+    #     return {num: sms[num]}
+    
+    def post(self, num):
+        name = request.form['name']
+        Response = demo_sms_send.send_sms(business_id=SMS_ID, phone_numbers=num, sign_name=SMS_SIGN, template_code=SMS_KEY, template_param={"name": name})
+        Response = Response.decode(encoding='UTF-8',errors='strict')
+
+        return Response
+
+
+api.add_resource(SMS_API, '/service/sms/<string:num>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
